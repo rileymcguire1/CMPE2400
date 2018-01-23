5209bdd1ae45da93c1c2e9e07b8ed45a086b57ac
Riley B Mcguire <rmcguire2@nait.ca>
2018-01-23 10:30:08 -0700

    changed ica5 to complete

--- ICAs/ica05_McguireRiley.sql
+++ ICAs/ica05_McguireRiley.sql
@@ -44,7 +44,7 @@ go
 --Q5
 select
 	left(Company, 48) as 'Company Name',
-	LastName as 'Name',
+	LastName as 'Contact',
 	left(Address, 36) as 'Street Address'
 from 
 	Customer
@@ -65,13 +65,13 @@ where
 go
 
 --Q7
-declare 
-	@time as time = dateadd(ms, Milliseconds, 0),
-	@cost as float
 select
 	TrackId as 'Track ID',
-	@time as 'Time',
-	@cost as 'Cost/Minute'
+	convert(time(3),DATEADD(ms, milliseconds, 0), 114) as 'Time',
+	cast(UnitPrice/(Milliseconds/1000.0/60.0) as money) as 'Cost/Minute',
+	cast(Bytes*1000.0/Milliseconds as decimal(8,3)) as 'Bytes/Second'
 from 
 	Track
+where
+	milliseconds>60000 and (UnitPrice/(Milliseconds/1000.0/60.0))>2.75
 go
