6f6781676e865dc503d8b27a3caa7331565b4eda
Riley B Mcguire <rmcguire2@nait.ca>
2018-04-09 14:51:43 -0600

    more procedures

--- Labs/lab01_McguireRiley.sql
+++ Labs/lab01_McguireRiley.sql
@@ -107,9 +107,7 @@ as
 	set @ErrorMessage = 'OK'
 	return 0;
 go
-declare @status as varchar(max)
-exec PopulateClass @status
-select @status
+
 
 --populate bike table with data
 if exists 
@@ -123,6 +121,7 @@ go
 create procedure PopulateBikes
 @ErrorMessage as varchar(max) output
 as
+	declare @bikeNumber as int = 0
 	if not exists 
 		(
 			select *
@@ -133,6 +132,88 @@ as
 			set @ErrorMessage = 'Bikes table does not exist'
 			return -1
 		end
-	
-	
+	while @bikeNumber < 20
+		begin
+			insert into Bikes(BikeID, StableDate)
+				values 
+					(format(@bikeNumber, '000') + 'Y-A', convert(date, getDate())),
+					(format(@bikeNumber, '000') + 'Y-P', convert(date, getDate())),
+					(format(@bikeNumber, '000') + 'H-A', convert(date, getDate())),
+					(format(@bikeNumber, '000') + 'H-P', convert(date, getDate())),
+					(format(@bikeNumber, '000') + 'Z-A', convert(date, getDate())),
+					(format(@bikeNumber, '000') + 'Z-P', convert(date, getDate()))
+			set @bikeNumber += 1
+		end
+	set @ErrorMessage = 'OK'
+	return 0;
 go
+
+--add a rider to the rider table
+if exists 
+(	
+	select *
+	from sysobjects
+	where [name] = 'AddRider'
+)
+drop procedure AddRider
+go
+create procedure AddRider
+@ErrorMessage as varchar(max) output,
+@Name as varchar(max),
+@ClassID as smallint
+as
+	declare @RiderID as smallint
+	if @Name is null
+		begin
+			set @ErrorMessage = 'Name can''t be NULL'
+			return -1
+		end	
+	if not exists(select * from Class where ClassID = @ClassID)
+		begin
+			set @ErrorMessage = 'ClassID ' + @ClassID + ' doesn''t exist'
+			return -1
+		end	
+	insert into Riders(Name, ClassID)
+		values (@name, @ClassID)
+	set @RiderID = @@IDENTITY
+	set @ErrorMessage = 'OK'
+	return @RiderID	
+go
+
+--Remove Rider from rider table
+if exists 
+(	
+	select *
+	from sysobjects
+	where [name] = 'RemoveRider'
+)
+drop procedure RemoveRider
+go
+create procedure RemoveRider
+@ErrorMessage as varchar(max) output,
+@RiderID as smallint,
+@Force as bit = false
+as
+	if @RiderID is null
+		begin
+			set @ErrorMessage = 'RiderID can''t be NULL'
+			return -1
+		end	
+	if not exists (select * from Riders where RiderID = @RiderID)
+		begin
+			set @ErrorMessage = 'RiderID ' + @RiderID + ' doesn''t exist'
+			return -1
+		end	
+	--check for sessions when force is false
+	if exists (select * from Sessions where RiderID = @RiderID)
+		begin
+			set @ErrorMessage = 'Can''t remove : currently in session'
+			return -1
+		end	
+go
+
+--execute all procedures
+
+declare @status as varchar(max)
+exec PopulateClass @status
+select @status
