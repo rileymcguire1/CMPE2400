6e776caefdab62213ee1175a007aef3b07b97340
Riley McGuire <31707588+rileymcguire1@users.noreply.github.com>
2018-04-11 21:22:55 -0600

    complete

--- Labs/lab01_McguireRiley.sql
+++ Labs/lab01_McguireRiley.sql
@@ -34,14 +34,14 @@ create table Class
 (
 	ClassID varchar(6)
 		constraint PK_Class_ClassID primary key,
-	ClassDescription varchar(45)
+	ClassDescription varchar(70)
 )
 
 create table Riders
 (
 	RiderID smallint identity (10, 1)
 		constraint PK_Riders_RiderID primary key,
-	Name varchar(64)
+	Name varchar(40)
 		constraint CHK_Riders_Name check (len (Name) > 4),
 	ClassID varchar(6)
 		constraint FK_Riders_ClassID foreign key
@@ -136,12 +136,12 @@ as
 		begin
 			insert into Bikes(BikeID, StableDate)
 				values 
-					(format(@bikeNumber, '000') + 'Y-A', convert(date, getDate())),
-					(format(@bikeNumber, '000') + 'Y-P', convert(date, getDate())),
-					(format(@bikeNumber, '000') + 'H-A', convert(date, getDate())),
-					(format(@bikeNumber, '000') + 'H-P', convert(date, getDate())),
-					(format(@bikeNumber, '000') + 'Z-A', convert(date, getDate())),
-					(format(@bikeNumber, '000') + 'Z-P', convert(date, getDate()))
+					(RIGHT('000'+CAST(@bikeNumber AS VARCHAR(6)),3) + 'Y-A', convert(date, getDate())),
+					(RIGHT('000'+CAST(@bikeNumber AS VARCHAR(6)),3) + 'Y-P', convert(date, getDate())),
+					(RIGHT('000'+CAST(@bikeNumber AS VARCHAR(6)),3) + 'H-A', convert(date, getDate())),
+					(RIGHT('000'+CAST(@bikeNumber AS VARCHAR(6)),3) + 'H-P', convert(date, getDate())),
+					(RIGHT('000'+CAST(@bikeNumber AS VARCHAR(6)),3) + 'S-A', convert(date, getDate())),
+					(RIGHT('000'+CAST(@bikeNumber AS VARCHAR(6)),3) + 'S-P', convert(date, getDate()))
 			set @bikeNumber += 1
 		end
 	set @ErrorMessage = 'OK'
@@ -160,7 +160,7 @@ go
 create procedure AddRider
 @ErrorMessage as varchar(max) output,
 @Name as varchar(max),
-@ClassID as smallint
+@ClassID as varchar(6)
 as
 	declare @RiderID as smallint
 	if @Name is null
@@ -170,7 +170,7 @@ as
 		end	
 	if not exists(select * from Class where ClassID = @ClassID)
 		begin
-			set @ErrorMessage = 'ClassID ' + @ClassID + ' doesn''t exist'
+			set @ErrorMessage = 'ClassID ' + cast(@ClassID as varchar(6)) + ' doesn''t exist'
 			return -1
 		end	
 	insert into Riders(Name, ClassID)
@@ -203,7 +203,7 @@ as
 	--RiderID does not exist in table
 	if not exists (select * from Riders where RiderID = @RiderID)
 		begin
-			set @ErrorMessage = 'RiderID ' + @RiderID + ' doesn''t exist'
+			set @ErrorMessage = 'RiderID ' + cast(@RiderID as varchar(4)) + ' doesn''t exist'
 			return -1
 		end	
 	--check for sessions when force is false
@@ -221,6 +221,7 @@ as
 	--delete rider with RiderID
 	delete Riders
 	where RiderID = @RiderID
+
 	set @ErrorMessage = 'OK'
 	return 0
 go
@@ -253,7 +254,7 @@ as
 			return -1
 		end	
 	--date is valid and greater than today
-	if isdate(@SessionDate) = 0 and @SessionDate >  getDate()
+	if @SessionDate >  getDate()
 		begin
 			set @ErrorMessage = 'Date must be valid'
 			return -1
@@ -261,23 +262,94 @@ as
 	--RiderID does not exist
 	if not exists (select * from Riders where RiderID = @RiderID)
 		begin
-			set @ErrorMessage = 'RiderID ' + @RiderID + ' doesn''t exist'
+			set @ErrorMessage = 'RiderID ' + cast(@RiderID as varchar(4)) + ' doesn''t exist'
 			return -1
 		end	
 	--BikeID does not exist
 	if not exists (select * from Bikes where BikeID = @BikeID)
 		begin
-			set @ErrorMessage = 'BikeID ' + @BikeID + ' doesn''t exist'
+			set @ErrorMessage = 'BikeID ' + cast(@BikeID as varchar(4)) + ' doesn''t exist'
 			return -1
 		end	
 	--Bike already assigned in session
-	if not exists (select * from Sessions where BikeID = @BikeID)
+	if exists (select * from Sessions where BikeID = @BikeID)
 		begin
-			set @ErrorMessage = 'BikeID ' + @BikeID + ' already assigned'
+			set @ErrorMessage = 'BikeID ' + cast(@BikeID as varchar(4)) + ' already assigned'
 			return -1
 		end	
 	insert into Sessions(RiderID, BikeID, SessionDate, Laps)
 	values (@RiderID, @BikeID, @SessionDate, 0)
+	set @ErrorMessage = 'OK'
+	return 0
+go
+
+--Update session in session table
+if exists 
+(	
+	select *
+	from sysobjects
+	where [name] = 'UpdateSession'
+)
+drop procedure UpdateSession
+go
+create procedure UpdateSession
+@ErrorMessage as varchar(max) output,
+@RiderID as smallint,
+@BikeID as varchar(6),
+@SessionDate as date,
+@Laps as int
+as
+	--RiderID is null
+	if @RiderID is null
+		begin
+			set @ErrorMessage = 'RiderID can''t be NULL'
+			return -1
+		end	
+	--BikeID is null
+	if @BikeID is null
+		begin
+			set @ErrorMessage = 'BikeID can''t be NULL'
+			return -1
+		end	
+	--date is null
+	if @SessionDate is null
+		begin
+			set @ErrorMessage = 'Date can''t be NULL'
+			return -1
+		end	
+	--Session does not exist
+	if not exists (	select * 
+					from Sessions 
+					where 
+						RiderID = @RiderID and 
+						BikeID = @BikeID and
+						SessionDate = @SessionDate
+					)
+		begin
+			set @ErrorMessage = 'Session doesn''t exist'
+			return -1
+		end	
+
+	--laps must be greater than current laps
+	if @Laps < (select Laps 
+					from Sessions 
+					where 
+						RiderID = @RiderID and
+						BikeID = @BikeID and
+						SessionDate = @SessionDate
+					)
+		begin
+			set @ErrorMessage = 'Laps must be greater than current laps'
+			return -1
+		end	
+
+	update Sessions
+	set Laps = @Laps
+	where 
+		RiderID = @RiderID and
+		BikeID = @BikeID and
+		SessionDate = @SessionDate 
+
 	set @ErrorMessage = 'OK'
 	return 0
 go
@@ -305,27 +377,24 @@ as
 	--ClassID does not exist
 	if not exists (select * from Class where ClassID = @ClassID)
 		begin
-			set @ErrorMessage = 'ClassID ' + @ClassID + ' doesn''t exist'
+			set @ErrorMessage = 'ClassID ' + cast(@ClassID as varchar(4)) + ' doesn''t exist'
 			return -1
 		end	
-	--determine riders in class to be deleted
-	select
-		@RID = r.RiderID
-	from
-		Class as c inner join Riders as r
-		on c.ClassID = r.ClassID
-	where
-		c.ClassID = @ClassID
 
-	--delete class, manually cascade to riders and sessions
+	----delete class, manually cascade to riders and sessions
 	delete Sessions
-	where RiderID = @RID
+	from
+		sessions as s inner join Riders as r
+		on s.RiderID = r.RiderID
+	where r.ClassID = @ClassID
 
 	delete Riders
-	where RiderID = @RID
+	where ClassID = @ClassID
 
 	delete Class
 	where ClassID = @ClassID
+
+	
 
 	set @ErrorMessage = 'OK'
 	return 0
@@ -360,7 +429,7 @@ as
 	--ClassID does not exist
 	if not exists (select * from Class where ClassID = @ClassID)
 		begin
-			set @ErrorMessage = 'ClassID ' + @ClassID + ' doesn''t exist'
+			set @ErrorMessage = 'ClassID ' + cast(@ClassID as varchar(4)) + ' doesn''t exist'
 			return -1
 		end	
 
@@ -416,20 +485,151 @@ as
 	--ClassID does not exist
 	if not exists (select * from Class where ClassID = @ClassID)
 		begin
-			set @ErrorMessage = 'ClassID ' + @ClassID + ' doesn''t exist'
+			set @ErrorMessage = 'ClassID ' + cast(@ClassID as varchar(4)) + ' doesn''t exist'
 			return -1
 		end	
 
 	--summary of class info 
 	select
-		--Count(distinct(BikeID,RiderID,SessionDate))
+		Count(distinct(s.BikeID +'|'+ cast(s.RiderID as varchar(3)) +'|'+ cast(s.SessionDate as varchar(max)))) as '# of sessions',
+		AVG(coalesce(Laps,0)) as 'Avg laps',
+		Min(coalesce(Laps,0)) as 'min laps',
+		Max(coalesce(Laps,0)) as 'max laps'
 	from
-		Riders left outer join Sessions
-		on Riders.RiderID = Sessions.RiderID
+		Riders as r left outer join Sessions as s
+		on r.RiderID = s.RiderID
+	where
+		r.ClassID = @ClassID or
+		(r.ClassID = '') and
+		(r.RiderID = @RiderID or
+		@RiderID = 0)
+
+	set @ErrorMessage = 'OK'
+	return 0
 go
 
---execute all procedures
+-----------------execute all procedures------------------------------------------------------------------------------------------------
 
 declare @status as varchar(max)
-exec PopulateClass @status
-select @status
+exec PopulateClass @status output
+print @status
+exec PopulateBikes @status output
+print @status
+go
+
+
+--AddRider testing----------------------
+declare @status as varchar(max)
+declare @Name as varchar(max) = 'Bobbie Smythe'
+declare @ClassID as varchar(6) = 'motogp'
+exec AddRider @status output, @Name, @ClassID
+print @status
+go
+
+declare @status as varchar(max)
+declare @Name as varchar(max) = 'Bobbie Smythe'
+declare @ClassID as varchar(6) = 'motogp'
+exec AddRider @status output, @Name, @ClassID
+print @status
+go
+
+declare @status as varchar(max)
+declare @Name as varchar(max) = 'Rob the man'
+declare @ClassID as varchar(6) = 'motogp'
+exec AddRider @status output, @Name, @ClassID
+print @status
+go
+
+declare @status as varchar(max)
+declare @Name as varchar(max) = 'Bobbie Smythe'
+declare @ClassID as varchar(6) = 'gofast'
+exec AddRider @status output, @Name, @ClassID
+print @status
+go
+
+--AddSession testing---------------------
+declare @status as varchar(max)
+declare @RiderID as smallint = 10
+declare @BikeID as varchar(6) = '010Y-A'
+declare @SessionDate as date = 'april 11 2018'
+exec AddSession @status output,@RiderID, @BikeID, @SessionDate
+print @status
+go
+
+declare @status as varchar(max)
+declare @RiderID as smallint = 10
+declare @BikeID as varchar(6) = '010Y-A'
+declare @SessionDate as date = 'april 11 2018'
+exec AddSession @status output,@RiderID, @BikeID, @SessionDate
+print @status
+go
+
+declare @status as varchar(max)
+declare @RiderID as smallint = 11
+declare @BikeID as varchar(6) = '001Y-A'
+declare @SessionDate as date = 'april 11 2018'
+exec AddSession @status output,@RiderID, @BikeID, @SessionDate
+print @status
+go
+
+--UpdateSession testing-------------------
+declare @status as varchar(max)
+declare @RiderID as smallint = 11
+declare @BikeID as varchar(6) = '001Y-A'
+declare @SessionDate as date = 'april 11 2018'
+declare @Laps as int = -1
+exec UpdateSession @status output,@RiderID, @BikeID, @SessionDate, @Laps
+print @status
+go
+
+declare @status as varchar(max)
+declare @RiderID as smallint = 11
+declare @BikeID as varchar(6) = '001Y-A'
+declare @SessionDate as date = 'april 11 2018'
+declare @Laps as int = 10
+exec UpdateSession @status output,@RiderID, @BikeID, @SessionDate, @Laps
+print @status
+go
+
+----RemoveRider testing--------------------
+declare @status as varchar(max)
+declare @RiderID as smallint = 10
+exec RemoveRider @status output, @RiderID
+print @status
+go
+
+declare @status as varchar(max)
+declare @RiderID as smallint = 10
+declare @Force as bit = 1
+exec RemoveRider @status output, @RiderID, @Force
+print @status
+go
+
+--ClassInfo testing--------------------------
+declare @status as varchar(max)
+declare @ClassID as varchar(6) = 'motogp'
+exec ClassInfo @status output, @ClassID
+print @status
+go
+
+declare @status as varchar(max)
+declare @ClassID as varchar(6) = 'moto_2'
+exec ClassInfo @status output, @ClassID
+print @status
+go
+
+--ClassSummary testing-----------------------
+declare @status as varchar(max)
+declare @ClassID as varchar(6) = 'motogp'
+declare @RiderID as smallint = 11
+exec ClassSummary @status output, @ClassID
+print @status
+go
+
+--RemoveClass testing------------------------
+declare @status as varchar(max)
+declare @ClassID as varchar(6) = 'motogp'
+exec RemoveClass @status output, @ClassID
+print @status
+go
+
